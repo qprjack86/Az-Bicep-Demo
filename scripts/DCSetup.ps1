78bbc0b5d229a82bd9a0c7c814d51bc5f9329a76
Jack Stalley <jack.stalley@cwlsystems.co.uk>
2022-09-15 15:14:45 +0100

    15:14

--- scripts/DCSetup.ps1
+++ scripts/DCSetup.ps1
@@ -9,9 +9,8 @@ choco install winscp -y
 choco install sysinternals -y
 choco install bginfo -y
 #Download Scripts to Set the rest of the Domain up when logged in
-New-Item -Path "c:\" -Name "BaselabSetup" -ItemType "directory" -Force
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/jakewalsh90/Terraform-Azure/main/Single-Region-Azure-BaseLab/PowerShell/baselab_DomainSetup.ps1" -OutFile "C:\BaselabSetup\baselab_DomainSetup.ps1"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/jakewalsh90/Terraform-Azure/main/Single-Region-Azure-BaseLab/PowerShell/baselab_LabStructure.ps1" -OutFile "C:\BaselabSetup\baselab_LabStructure.ps1"
+New-Item -Path "c:\" -Name "LabSetup" -ItemType "directory" -Force
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/qprjack86/Az-Bicep-Demo/main/scripts/DomainSetup.ps1" -OutFile "C:\LabSetup\lab_DomainSetup.ps1"
 #Setup and Partition Data Disk
 #Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel 'Data' -Confirm:$false 
 #Allow Ping
